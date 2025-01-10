d4aa14755e31ffb615eac13e99e64ffdf2201ba8
frncscp <franciscocerenporto@gmail.com>
2025-01-09 23:25:59 -0500

    Automatic Update

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -26,14 +26,14 @@ public class Tesis extends Escrito{
 
     @Override
     public String toString(){
-        return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.idea + "\n" + this.argumentos.size() + "\n" + this.conclusion + "\n" + this.referencias + "\n" + this.interpretacion;
+        return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.idea + "\n" + this.argumentos.length + "\n" + this.conclusion + "\n" + this.referencias + "\n" + this.interpretacion;
     }
 
     public String getIdea(){return this.idea;}
     public void setIdea(String idea){this.idea = idea;}
 
-    public String getArgumentos(){return this.argumentos;}
-    public void setArgumentos(String argumentos){this.argumentos = argumentos;}
+    public String[] getArgumentos(){return this.argumentos;}
+    public void setArgumentos(String[] argumentos){this.argumentos = argumentos;}
 
     public String getConclusion(){return this.conclusion;}
     public void setConclusion(String conclusion){this.conclusion = conclusion;}
